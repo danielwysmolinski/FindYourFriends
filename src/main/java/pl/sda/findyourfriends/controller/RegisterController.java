fa0c419924b436fa17e3de355070d2e31796c5af
Wojciech Krzyzanowski <krzyzanowski.wojciech@outlook.com>
2020-07-12 16:13:26 +0200

    Signup form fixed

--- src/main/java/pl/sda/findyourfriends/controller/RegisterController.java
+++ src/main/java/pl/sda/findyourfriends/controller/RegisterController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.findyourfriends.repository.UserRepository;
 import pl.sda.findyourfriends.users.User;
 
@@ -22,18 +19,18 @@ public class RegisterController {
     private UserRepository userRepository;
 
 
-
-        @GetMapping("/signup")
-        public String showSignUpForm() {
-            return "add-user";
-        }
-
-        @PostMapping("/adduser")
-        public String addUser(@Valid @RequestBody User user) {
-
-
-            userRepository.save(user);
-            return "redirect:/hello";
-        }
-
+    @GetMapping("/signup")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+        return "add-user";
     }
+
+    @PostMapping(path = "/adduser", consumes = {"application/x-www-form-urlencoded"})
+    public String addUser(@Valid @ModelAttribute User user) {
+
+
+        userRepository.save(user);
+        return "redirect:/hello";
+    }
+
+}
--- src/main/java/pl/sda/findyourfriends/controller/WebSecurityConfig.java
+++ src/main/java/pl/sda/findyourfriends/controller/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/home", "/signup").permitAll()
+				.antMatchers("/", "/home", "/signup", "/adduser").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
