22750b83d8f44af3982cb23c35c53ed3e8c54c27
Daniel Wysmoliński <dwysmolinski@gmail.com>
2020-07-12 16:16:52 +0200

    modyfication LoginController userController WebSecurityConfig and User

--- src/main/java/pl/sda/findyourfriends/controller/LoginController.java
+++ src/main/java/pl/sda/findyourfriends/controller/LoginController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+
     @RequestMapping(path = "/login")
     public String login() {
         return "login";
+    }
+
+    @RequestMapping(path = "/")
+    public String indexHtml() {
+        return "home";
     }
 }
--- src/main/java/pl/sda/findyourfriends/controller/UserController.java
+++ src/main/java/pl/sda/findyourfriends/controller/UserController.java
@@ -1,7 +1,6 @@
 package pl.sda.findyourfriends.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.findyourfriends.repository.UserRepository;
 import pl.sda.findyourfriends.users.User;
