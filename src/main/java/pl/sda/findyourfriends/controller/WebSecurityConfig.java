e87e41a4c0747d51c0c9073fca6d87924a4f77f6
Daniel Wysmoliński <dwysmolinski@gmail.com>
2020-07-18 15:26:20 +0200

    websecurity config without comment

--- src/main/java/pl/sda/findyourfriends/controller/WebSecurityConfig.java
+++ src/main/java/pl/sda/findyourfriends/controller/WebSecurityConfig.java
@@ -31,20 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+				User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
 
-
-
-
-//	@Bean
-//	@Override
-//	public UserDetailsService userDetailsService() {
-//		UserDetails user =
-//				User.withDefaultPasswordEncoder()
-//				.username("user")
-//				.password("password")
-//				.roles("USER")
-//				.build();
-//
-//		return new InMemoryUserDetailsManager(user);
-//    }
+		return new InMemoryUserDetailsManager(user);
+    }
 }
