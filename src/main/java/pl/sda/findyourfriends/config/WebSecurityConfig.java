32e7a3ba6209c1bfb0ac3155a3bfa6e83e139f79
Anastaszja <nastya.chekh96@gmail.com>
2020-07-25 09:22:00 +0200

    pre-heroku

--- src/main/java/pl/sda/findyourfriends/config/WebSecurityConfig.java
+++ src/main/java/pl/sda/findyourfriends/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http.authorizeRequests()
-            .antMatchers("/", "/home", "/signup", "/login").permitAll()
+            .antMatchers("/", "/home", "/signup", "/adduser").permitAll()
             .anyRequest().authenticated()
         .and()
             .formLogin()
